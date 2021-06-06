e710b86367394c139d0d0f5d19c5b14de84253d4
KulaevAnton <kulaev-antyan@mail.ru>
2021-06-07 00:01:35 +0300

    console.log

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,42 @@
-//here will by JavaScript code ...
+// here will by JavaScript code ...
+// one line comments
+/*
+* many lines 
+* comment
+*/
+
+// boolean operators AND (&&) OR (||) NOT (!)
+
+var a = false;
+var b = true;
+
+console.log('a = false b = true');
+
+console.log('a && b', a && b);
+console.log('!a && b', !a && b);
+console.log('!a && !b', !a && !b);
+
+
+console.log('a || b', a || b);
+console.log('!a || b', !a || b);
+console.log('a || !b', a || !b);
+
+// conditional operators
+
+// if () {
+
+// } else if {
+
+// } else {
+
+// }
+
+var foo = 3;
+if (foo < 4) {
+    console.log('foo less then 4')
+}
+
+var foo = 5;
+if (foo < 8 && foo > 4) {
+    console.log('foo less then 8 and more 4')
+}
