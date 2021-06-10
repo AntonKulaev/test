d5fbe13fed527abf814dd14a9befe3b1ba28b712
KulaevAnton <kulaev-antyan@mail.ru>
2021-06-10 22:32:07 +0300

    js

--- src/js/main.js
+++ src/js/main.js
@@ -98,14 +98,73 @@ console.log(tmp);
 
 // Loops
 
-var i = 0;
-for(i; i < 10; i++) {
-    if (i === 4) {
+// var i = 0;
+// for(i; i < 10; i++) {
+//     if (i === 4) {
+//         continue;
+//     }
+//     console.log(i);
+
+//     if (i === 5) {
+//         break;
+//     }   
+// }
+
+i = 2;
+for (i; i < 15; i++) {
+    if (i === 7) {
         continue;
     }
     console.log(i);
 
-    if (i === 5) {
+    if (i === 14) {
         break;
-    }   
+    }
 }
+
+i = 7;
+while (i < 12) {
+    console.log('while', i++);
+}
+
+i = 0;
+do {
+    console.log('do while', i++);
+} while (i < 25);
+
+
+
+function fnfoo(param1, param2) {
+    var foo = param1 * 10;
+    var boo = param2 * 10;
+
+    return boo + foo;
+}
+
+console.log ('fnfoo(2,3) = ', fnfoo (2,3));
+
+
+var fnBoo = function() {
+    console.log('function expression');
+}
+
+fnBoo()
+
+function fibonnachi(n) {
+    if (n === 2 ) {
+        return 1;
+    }
+    if (n <= 1) {
+        return 0;
+    }
+    return fibonnachi(n - 1) + fibonnachi(n - 2);
+}
+for ( var i = 1; i< 10; i++) {
+    console.log(fibonnachi(i));
+}
+
+
+var tmp = document.querySelector('p');
+tmp.classList.add('warning');
+
+tmp.innerHTML = '*\n**\n***\n****\n*****';
